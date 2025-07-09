e4dd10700c8bdeb59870d3f34cb1be914178c4b2
crypto-cooker <mateo.kova0510@gmail.com>
2025-07-09 18:31:05 +0000

    fix: tg login

--- widgets/TelegramLoginButton.tsx
+++ widgets/TelegramLoginButton.tsx
@@ -1,25 +1,19 @@
-import { useEffect, useRef } from 'react';
-import { WalletAuthUser } from "@/lib/types";
+import React, { useEffect } from 'react';
 
-export const TelegramLoginButton = ({ onAuth }: { onAuth?: (user: WalletAuthUser) => void }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
+export const TelegramLoginButton = () => {
   useEffect(() => {
-    if (ref.current) ref.current.innerHTML = '';
-    // Define the global callback
-    (window as any).onTelegramAuth = (user: any) => {
-      if (onAuth) onAuth(user);
+    (window as any).onTelegramAuth = function(user: any) {
+      alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
     };
-    const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.setAttribute('data-telegram-login', 'MyVibinBot'); // your bot username, no @
-    script.setAttribute('data-size', 'large');
-    script.setAttribute('data-radius', '10');
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-    script.setAttribute('data-request-access', 'write');
-    script.async = true;
-    ref.current?.appendChild(script);
-  }, [onAuth]);
+  }, []);
 
-  return <div ref={ref}></div>;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="MyVibinBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+        `
+      }}
+    />
+  );
 }; 
