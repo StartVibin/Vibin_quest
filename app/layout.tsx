33879d29127d87a87265a6b639d4b6fc59359a81
crypto-cooker <mateo.kova0510@gmail.com>
2025-07-09 20:10:22 +0000

    fix; tg oauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Header } from "@/widgets/Header";
 import Web3Provider from "@/provider/WagmiProvider";
 import ReactQueryProvider from "@/provider/ReactQueryProvider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Vibin'",
@@ -30,11 +31,27 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
+                <Script id="telegram-auth-callback">
+                    {`
+                        function onTelegramAuth(user) {
+                            alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+                        }
+                    `}
+                </Script>
                 <ReactQueryProvider>
                     <Web3Provider>
                         <Header />
 
                         {children}
+                        <Script
+                            async
+                            src="https://telegram.org/js/telegram-widget.js?22"
+                            data-telegram-login="OAuth_By_Telegram_Bot" // <-- your bot username, no @
+                            data-size="large"
+                            data-radius="5"
+                            data-onauth="onTelegramAuth(user)"
+                            data-request-access="write"
+                        />
                     </Web3Provider>
                 </ReactQueryProvider>
             </body>
